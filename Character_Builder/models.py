1656622c43156622c4c30901dc11e9263a5dfa7a
DelgadoJosh <detjaydee803@gmail.com>
2019-02-27 20:11:31 -0500

    Added notes to the character builder's model file. Also binaries changed

--- Character_Builder/models.py
+++ Character_Builder/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Notes (Django models)
+# Each model acts more or less like a database table
+# Each model's field acts like a column in said table
+# Foreign Keys act as a thing that links a class
+# to a parent class that uses it. 
+#  eg: 
+
+
+# Description of this model file
+# Much of this will be based off of the database schemas
+
+# As this is in the character builder folder, this will focus on
+# the character information 
