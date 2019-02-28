b45d3c0e7c5db628d658b61d9972bd0487d73e84
DelgadoJosh <detjaydee803@gmail.com>
2019-02-28 12:15:56 -0500

    Finished early draft of character class

--- Character_Builder/models.py
+++ Character_Builder/models.py
@@ -10,9 +10,27 @@ from django.db import models
 #  eg: abilityScore's "characterID" is a foreign key
 
 
+# Constants
+max_length_name = 255
+max_length_alignment = 255
+max_length_size = 255
+
+
 # Description of this model file
 # Much of this will be based off of the database schemas
 
 # As this is in the character builder folder, this will focus on
 # the character information 
+
+class Character(models.Model):
+    characterID = models.IntegerField()
+    raceID = models.IntegerField()
+    classID = models.IntegerField()
+    name = models.CharField(max_length=max_length_name)
+    abilityScoreSetID = models.IntegerField()
+    level = models.IntegerField()
+    xp = models.IntegerField()
+    hp = models.IntegerField()
+    alignment = models.CharField(max_length = max_length_alignment)
+    size = models.CharField(max_length = max_length_size)
 
