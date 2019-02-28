6cb8536466895bbed2db9cd95a20caddee48c65b
DelgadoJosh <detjaydee803@gmail.com>
2019-02-28 12:50:59 -0500

    Capitalized constants

--- Character_Builder/models.py
+++ Character_Builder/models.py
@@ -11,16 +11,17 @@ from django.db import models
 
 
 # Constants
-# Should I capitalize this?
 # Also should I move this next to the class that uses it?
-max_length_character_name = 255
-max_length_alignment = 255
-max_length_size = 255
+MAX_LENGTH_CHARACTER_NAME = 255
+MAX_LENGTH_ALIGNMENT = 255
+MAX_LENGTH_SIZE = 255
 
-max_length_ability_name = 255
+MAX_LENGTH_ABILITY_NAME = 255
 
-max_length_class_name = 255
-max_length_hit_dice = 255
+MAX_LENGTH_CLASS_NAME = 255
+MAX_LENGTH_HIT_DICE = 255
+
+MAX_LENGTH_RACE_NAME = 255
 
 
 # Description of this model file
@@ -34,13 +35,13 @@ class Character(models.Model):
     characterID = models.IntegerField()
     raceID = models.IntegerField()
     classID = models.IntegerField()
-    characterName = models.CharField(max_length = max_length_character_name) # Is this a consistent level of abstraction?
+    characterName = models.CharField(max_length = MAX_LENGTH_CHARACTER_NAME) # Is this a consistent level of abstraction?
     abilityScoreSetID = models.IntegerField()
     level = models.IntegerField()
     xp = models.IntegerField()
     hp = models.IntegerField()
-    alignment = models.CharField(max_length = max_length_alignment) # Use string or an enum?
-    size = models.CharField(max_length = max_length_size) # Use string or enum?
+    alignment = models.CharField(max_length = MAX_LENGTH_ALIGNMENT) # Use string or an enum?
+    size = models.CharField(max_length = MAX_LENGTH_SIZE) # Use string or enum?
 
 # This class is dynamic, the abilityScoreValues may change
 class AbilityScoreSet(models.Model):
@@ -51,18 +52,18 @@ class AbilityScoreSet(models.Model):
 # This class is static, like a lookup table
 class AbilityScore(models.Model):
     abilityScoreID = models.foreignKey()  # TODO: Again, need to research this
-    abilityName = models.CharField(max_length = max_length_ability_name)
+    abilityName = models.CharField(max_length = MAX_LENGTH_ABILITY_NAME)
 
 # This class is largely static, like a lookup table
 class CharacterClass(models.Model):
     classID = models.foreignKey()   # TODO: Research this
-    className = models.CharField(max_length = max_length_class_name)
-    hitDice = models.CharField(max_length = max_length_hit_dice)
+    className = models.CharField(max_length = MAX_LENGTH_CLASS_NAME)
+    hitDice = models.CharField(max_length = MAX_LENGTH_HIT_DICE)
 
 # This class is largely static, like a lookup table
 class CharacterRace(models.Model):
     raceID = models.foreignKey()    # TODO: Research this
-    raceName = models.CharField(max_length = max_length_race_name)
+    raceName = models.CharField(max_length = MAX_LENGTH_RACE_NAME)
     abilityScoreBonusSetID = models.IntegerField()  # Same level of abstraction?
     speed = models.IntegerField()
     size = models.CharField(max_length_size)   # Okay to overload?
