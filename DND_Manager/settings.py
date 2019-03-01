417a5a6bfd2ed9f9571b75e30062484634d489a7
SpinksJared <47286783+SpinksJared@users.noreply.github.com>
2019-03-01 18:21:31 -0500

    Added Base Class HTML with Bootstrapping and CSS styling

--- DND_Manager/settings.py
+++ DND_Manager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Character_Builder.apps.CharacterBuilderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
