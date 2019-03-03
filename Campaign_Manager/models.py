9c89fd4b762630a8b7211cebc21af9d46d159bd6
Elocutioner <jacksonn1717@yahoo.com>
2019-03-02 23:48:22 -0500

    Update models.py

--- Campaign_Manager/models.py
+++ Campaign_Manager/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+#
+class Campaign(models.Model):
+    CampaignID = models.IntegerField(unique=True)
+    CampaignDMID = models.IntegerField()
+    PartyID = models.IntegerField()
+
+#
+class CampaignDM(models.Model):
+    CampaignDMID = models.IntegerField(unique=True)
+    CharacterID = models.IntegerField()
+
+#
+class Party(models.Model):
+    PartyID = models.IntegerField(unique=True)
+    PartyCharactersID = models.IntegerField()
+
+#
+class PartyCharacters(models.Model):
+    PartyID = models.IntegerField()
+    CharacterID = models.IntegerField()
+
