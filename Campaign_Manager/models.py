d197dc5d0d173b84ceb8aa7a804abd52ec67cdff
Elocutioner <jacksonn1717@yahoo.com>
2019-03-03 17:05:54 -0500

    added campaign models

--- Campaign_Manager/models.py
+++ Campaign_Manager/models.py
@@ -1,25 +1,27 @@
 from django.db import models
+from Character_Builder.models import Character
 
-# Create your models here.
+# Constants
+MAX_LENGTH_CAMPAIGN_NAME = 255
 
-#
+# Keeps track of individual campaigns
 class Campaign(models.Model):
     CampaignID = models.IntegerField(unique=True)
-    CampaignDMID = models.IntegerField()
-    PartyID = models.IntegerField()
+    CampaignName = models.CharField(max_length = MAX_LENGTH_CAMPAIGN_NAME)
+    CampaignDM = models.OneToOneField(CampaignDM, on_delete=models.CASCADE)
+    Party = models.OneToOneField(Party, on_delete=models.CASCADE)
 
-#
+# Keeps track of DMs
 class CampaignDM(models.Model):
     CampaignDMID = models.IntegerField(unique=True)
-    CharacterID = models.IntegerField()
+    Character = models.OneToOneField(Character, on_delete=models.CASCADE)
 
-#
+# Keeps track of parties
 class Party(models.Model):
     PartyID = models.IntegerField(unique=True)
-    PartyCharactersID = models.IntegerField()
 
-#
-class PartyCharacters(models.Model):
-    PartyID = models.IntegerField()
-    CharacterID = models.IntegerField()
+# used to allow parties to store mulitple party members
+class PartyCharacter(models.Model):
+    Party = models.ForeignKey(Party, on_delete=models.CASCADE)
+    Character = models.ForeignKey(Character, on_delete=models.CASCADE)
 
