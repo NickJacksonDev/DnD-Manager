edcb217c15e0de249f4243bc65277f2be1af14e4
Elocutioner <jacksonn1717@yahoo.com>
2019-03-05 15:09:37 -0500

    Can now add campaigns to the DB via the site

--- Campaign_Manager/views.py
+++ Campaign_Manager/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from .models import Campaign
 from Character_Builder.models import Character
 from django.http import HttpResponse
-
-
+from django.http import HttpResponseRedirect
+from .forms import CampaignForm
 
 # Create your views here.
 def home(request):
+    form = CampaignForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
 
     context = {
             'campaigns' : Campaign.objects.all(),
             'title' : 'Campaigns',
             'characters' : Character.objects.all(),
+            'form' : form,
     }
 
     return render(request, 'Campaign_Manager/campaign_manager-home.html', context)
