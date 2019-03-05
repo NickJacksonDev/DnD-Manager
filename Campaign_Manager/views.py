e8e3f45b8dae61f2485776be9d6b3ce60f2278ea
SpinksJared <47286783+SpinksJared@users.noreply.github.com>
2019-03-04 22:01:14 -0500

    Added Database query printouts

--- Campaign_Manager/views.py
+++ Campaign_Manager/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Campaign
+from Character_Builder.models import Character
 from django.http import HttpResponse
+
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'Campaign_Manager/campaign_manager-home.html', {'title': 'Campaigns'})
+
+    context = {
+            'campaigns' : Campaign.objects.all(),
+            'title' : 'Campaigns',
+            'characters' : Character.objects.all(),
+    }
+
+    return render(request, 'Campaign_Manager/campaign_manager-home.html', context)
