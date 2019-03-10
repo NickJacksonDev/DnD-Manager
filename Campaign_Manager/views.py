6aabb697a1bcc68530e5dd3c7bafaf48dd4f304d
NickJacksonDev <47230567+NickJacksonDev@users.noreply.github.com>
2019-03-09 20:27:34 -0500

    Update views.py

--- Campaign_Manager/views.py
+++ Campaign_Manager/views.py
@@ -4,6 +4,7 @@ from Character_Builder.models import Character
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from .forms import CreateCampaignForm
+from django.views.generic import CreateView
 
 # Create your views here.
 def home(request):
@@ -20,3 +21,11 @@ def home(request):
     }
 
     return render(request, 'Campaign_Manager/campaign_manager-home.html', context)
+
+class CampaignCreateView(CreateView):
+    model = Campaign
+    fields = ['campaignName']
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
