fbe023c7a0ca89fddbe21aab7d0d609fee2b5278
NickJacksonDev <47230567+NickJacksonDev@users.noreply.github.com>
2019-03-18 23:07:04 -0400

    Inventory models and html

--- Inventory/forms.py
+++ Inventory/forms.py
@@ -1,1 +1,7 @@
 from django import forms
+from .models import Item
+
+class CreateItemForm(forms.ModelForm):
+	class Meta:
+		model = Item
+		fields = ['itemName']
--- Inventory/models.py
+++ Inventory/models.py
@@ -5,20 +5,29 @@ from django.contrib.auth.models import User
 # Constants
 MAX_LENGTH_ITEM_NAME = 255
 
-# finds a default user
-def defaultUser():
-    default = User.objects.first()
-
-    if default is None:
-        default = User.objects.create_user('defaultUser', password='djangoproject')
-
+# Creates a default character
+def defaultCharacter():
+    default = Character.objects.first()
     return default
 
-# Keeps track of individual campaigns
+# Keeps track of inventories
+class Inventory(models.Model):
+    inventoryID = models.AutoField(primary_key=True)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, default=defaultCharacter)
+
+    def __str__(self):
+        return self.character.characterName
+
+# Creates default inventory
+def defaultInventory():
+    default = Inventory.objects.first()
+    return default
+
+# Keeps track of individual items
 class Item(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=defaultUser)
     itemID = models.AutoField(primary_key=True)
     itemName = models.CharField(max_length = MAX_LENGTH_ITEM_NAME)
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE, default=defaultInventory)
 
     def __str__(self):
         return self.itemName
