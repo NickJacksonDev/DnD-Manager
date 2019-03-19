aad703c59b00ee90435f012a9e61a33ceec3be43
Benjamin Rhiner <rhinerb@yahoo.com>
2019-03-18 22:38:37 -0400

    Created the ability for users to update their profile - Form to update is currently in the profile page - Form auto-populates with user information - Uploading a new profile picture will resize to a maximum width or height of 300 before saving

--- Users/models.py
+++ Users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,13 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+	def save(self):
+		super().save()
+
+		image = Image.open(self.image.path)
+
+		if image.width > 300 or image.height > 300:
+			output_size = (300, 300)
+			image.thumbnail(output_size)
+			image.save(self.image.path)
