e22aadaccf426721869affbb10ea7b898cbd5bb5
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2019-03-18 22:36:26 -0400

    Updated urls and character_list

--- Character_Builder/urls.py
+++ Character_Builder/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('', views.home, name='character_builder-home'),
     # path('', PostListView.as_view(), name='character_builder-home'),
     path('characters/', CharacterListView.as_view(), name='character-list'),
-    path('characters/<int:pk>/', CharacterDetailView.as_view(), name='character-detail'),
     path('characters/create/', CharacterCreateView.as_view(), name='character-create'),
+    path('characters/<int:pk>/', CharacterDetailView.as_view(), name='character-detail'),
     path('characters/<int:pk>/edit', CharacterEditView.as_view(), name='character-edit'),
     path('characters/<int:pk>/delete', CharacterDeleteView.as_view(), name='character-delete'),
 ]
